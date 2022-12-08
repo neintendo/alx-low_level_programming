1755e0f0eaf00bf1104444bc5c2a235043c90eaa
Alfredo Kambasha <kambashaalfredo@gmail.com>
2022-12-08 23:00:08 +0200

    Created 4-puts.c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string of text.
+ * Description: Prints text in text[] array using puts to stdout.
+ * Return: "Programming is like building a multilingual puzzle"
+ */
+int main(void)
+{
+	char text[] = {"\"Programming is like building a multilingual puzzle"};
+
+	puts(text);
+	return (0);
+}
