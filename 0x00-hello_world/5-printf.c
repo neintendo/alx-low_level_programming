0c5a995bb0da24cff88d87fef98b9e5c63d2211d
Alfredo Kambasha <kambashaalfredo@gmail.com>
2022-12-08 23:39:57 +0200

    Created 5-printf.c program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints out text.
+ * Return: prints out "with proper grammar, but the outcome is a piece of art,"
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
