20e884bd89121dba2dc5d89871c1b936eda260f8
Alfredo Kambasha <kambashaalfredo@gmail.com>
2022-12-16 20:55:17 +0200

    created 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * more_numbers - prints 1 to 14
+ *
+ * Description: prints numbers 1 to 14, 10 times.
+ */
+void more_numbers(void)
+{
+	int one, two, three, loop;
+
+	loop = 1;
+
+	do {
+		for (one = 0; one < 10; one++)
+	{
+		_putchar(one + '0');
+
+		if (one == 9)
+		{
+			for (three = 1, two = 0; two < 5; two++)
+			{
+				_putchar(three + '0');
+				_putchar(two + '0');
+			}
+		}
+	}
+		write(1, "\n", 2);
+		loop++;
+	} while (loop <= 10);
+}
