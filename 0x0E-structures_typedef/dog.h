a68713f8e09bd0d3bdff30a1a3b05cde27bebc57
Alfredo Kambasha <kambashaalfredo@gmail.com>
2023-01-17 00:13:08 +0200

    created 1-init_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,15 +3,18 @@
 
 /**
  * struct dog - structure for dog identities.
+ *
  * @name: the dog's name.
  * @age: the dog's age.
  * @owner: the dog's owner.
-*/
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
