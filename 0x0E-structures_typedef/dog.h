943821c781c1b7c6301e6aadaee19b4117a43a51
Alfredo Kambasha <kambashaalfredo@gmail.com>
2023-01-16 23:27:51 +0200

    created dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dog identities.
+ * @name: the dog's name.
+ * @age: the dog's age.
+ * @owner: the dog's owner.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
