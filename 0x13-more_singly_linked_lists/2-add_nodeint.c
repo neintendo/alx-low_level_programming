c27014b49de7302203eb05f44b854e7379520ae4
Alfredo Kambasha <kambashaalfredo@gmail.com>
2023-02-01 21:56:04 +0200

    created 2-add_nodeint.c

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdarg.h>
+
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
+ * @head: the first node.
+ * @n: integer in the current node.
+ *
+ * Return: returns listint_t with new node.
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -8,3 +8,8 @@
 **1-listint_len.c:** returns the number of elements in a linked listint_t list.
 
 - Prototype: size_t listint_len(const listint_t *h);
+
+**2-add_nodeint.c:** adds a new node at the beginning of a listint_t list.
+
+- Prototype: listint_t *add_nodeint(listint_t **head, const int n);
+- Return: the address of the new element, or NULL if it failed
