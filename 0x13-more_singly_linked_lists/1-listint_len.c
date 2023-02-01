c95783368b253b0ad4a0d5d7301f43dbd66cd6ea
Alfredo Kambasha <kambashaalfredo@gmail.com>
2023-02-01 14:11:31 +0200

    created 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,14 +1,13 @@
 #include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * print_listint - prints all elements of a listint_t list.
+ * listint_len - returns the number of elements in a linked listint_t list.
  * @h: the first node.
  *
- * Return: returns number of nodes.
+ * Return: number of nodes.
 */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	listint_t *temp;
 	unsigned int nodes = 0;
@@ -16,7 +15,6 @@ size_t print_listint(const listint_t *h)
 	while (h != NULL)
 	{
 		temp = h->next;
-		printf("%d\n", h->n);
 		nodes++;
 		h = temp;
 	}
--- 0x13-more_singly_linked_lists/README.md
+++ 0x13-more_singly_linked_lists/README.md
@@ -4,3 +4,7 @@
 
 - Prototype: size_t print_listint(const listint_t *h);
 - Return: the number of nodes
+
+**1-listint_len.c:** returns the number of elements in a linked listint_t list.
+
+- Prototype: size_t listint_len(const listint_t *h);
