37cb57708a25cbba5c4ca68795750317795b4358
Alfredo Kambasha <kambashaalfredo@gmail.com>
2023-07-18 00:12:52 +0200

    project re-upload

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,8 @@
+#ifndef ABS_H
+#define ABS_H
+/**
+ * ABS - computes the absolute value of a number x.
+ * @x: the number to find the absolute value of.
+*/
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+#endif
